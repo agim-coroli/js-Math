5f05ea6d1c5b5fdd9183dc02d78fc12ab2decf1c
Agim Coroli <agim.coroli8016@gmail.com>
2021-05-04 14:08:18 +0100

    Math

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+//###1 let nbr = 8.56 
+let nbr = 8.56;
+
+//###2 arondir au plus proche
+console.log(Math.round(nbr));
+
+//###3 arondir au plus haut
+console.log(Math.ceil(nbr));
+
+//###4 arondir au plus bas
+console.log(Math.floor(nbr));
+
+//###5 retirer la parti decimal
+console.log(Math.trunc(nbr));
+
+//###6 afficher un nombre aleatoir entre 0 et 1
+console.log(Math.random(nbr));
+
+//###7 afficher un nombre aleatoire entre 0 et 100
+console.log(Math.random(nbr)*100);
+
+//###8 afficher un nombre entier aleatoire entre 0 et 100
+console.log(Math.round(Math.random()*100));
+
+//###9 afficher 8 puissance 2
+console.log(Math.pow(8,2));
+
+//###10 renvoi la racine carré de 9
+console.log(Math.sqrt(9));
+
+//###11 retourne la valeur absolue d'un nombre,
+console.log(Math.abs(-1));
+
+//###12 renvoie le plus grand nombre d'une série de 0 ou plusieurs nombres
+console.log(Math.max(-2, 1000, 8, 57));
+
+//###13 renvoie le plus petit nombre d'une série de 0 ou plusieurs nombres
+console.log(Math.min(-2, 1000, 8, 57));
+
